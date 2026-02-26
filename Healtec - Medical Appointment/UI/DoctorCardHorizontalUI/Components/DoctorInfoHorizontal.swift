a31fe068a6bcfa5e90cee107c42ae6caa88e9ee2
Export <smg05892@gmail.com>
2026-02-27 01:42:11 +0300

    add doctor horizontal card

---
+++ Healtec - Medical Appointment/UI/DoctorCardHorizontalUI/Components/DoctorInfoHorizontal.swift
@@ -0,0 +1,98 @@
+import UIKit
+
+class DoctorInfoHorizontalView: UIView {
+    
+    let doctorName: UILabel = {
+        let lbl = UILabel()
+        lbl.font = UIFont(name: "Inter-SemiBold", size: 16)
+        lbl.textColor = .textBlack
+        return lbl
+    }()
+    
+    let doctorSpeciality: UILabel = {
+        let lbl = UILabel()
+        lbl.font = UIFont(name: "Inter-Light", size: 12)
+        lbl.textColor = .textGrey
+        return lbl
+    }()
+    
+    private let starIcon: UIImageView = {
+        let img = UIImageView()
+        img.image = UIImage(named: "star")
+        return img
+    }()
+    
+    let doctorRaiting: UILabel = {
+        let lbl = UILabel()
+        lbl.font = UIFont(name: "Inter-Medium", size: 12)
+        lbl.textColor = .textBlack
+        return lbl
+    }()
+    
+    
+    private lazy var doctorNameStack: UIStackView = {
+        let stack = UIStackView(arrangedSubviews: [doctorName, doctorSpeciality])
+        stack.axis = .vertical
+        stack.spacing = 4
+        stack.distribution = .fill
+        stack.alignment = .leading
+        return stack
+    }()
+    
+    private lazy var doctorRaitingStack: UIStackView = {
+        let stack = UIStackView(arrangedSubviews: [starIcon, doctorRaiting])
+        stack.axis = .horizontal
+        stack.spacing = 4
+        stack.distribution = .fill
+        stack.alignment = .center
+        return stack
+    }()
+    
+    private lazy var mainStack: UIStackView = {
+        let stack = UIStackView(arrangedSubviews: [doctorNameStack, doctorRaitingStack])
+        stack.axis = .vertical
+        stack.spacing = 16
+        stack.distribution = .equalSpacing
+        stack.alignment = .top
+        return stack
+    }()
+
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupUI()
+        setupConstrains()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func configure(name: String, speciality: String, raiting: Double) {
+        doctorName.text = name
+        doctorSpeciality.text = speciality
+        doctorRaiting.text = "\(raiting)"
+    }
+    
+    private func setupUI() {
+        addSubview(mainStack)
+    }
+    
+    private func setupConstrains() {
+        [mainStack].forEach {
+            $0.translatesAutoresizingMaskIntoConstraints = false
+        }
+                
+        NSLayoutConstraint.activate([
+            starIcon.heightAnchor.constraint(equalToConstant: 18),
+            starIcon.widthAnchor.constraint(equalToConstant: 18),
+            
+            mainStack.leadingAnchor.constraint(equalTo: self.leadingAnchor),
+            mainStack.trailingAnchor.constraint(equalTo: self.trailingAnchor),
+            mainStack.centerYAnchor.constraint(equalTo: self.centerYAnchor)
+        ])
+
+
+    }
+}
+
---
+++ Healtec - Medical Appointment/UI/DoctorCardHorizontalUI/DoctorCardHorizontalUI.swift
@@ -0,0 +1,64 @@
+import UIKit
+
+class DoctorCardHorizontalUIView: UIView {
+    
+    let doctorInfoView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .white
+        view.layer.shadowColor = UIColor(red: 151/255, green: 151/255, blue: 150/255, alpha: 1).cgColor
+        view.layer.shadowOpacity = 0.14
+        view.layer.shadowOffset = CGSize(width: 0, height: -5)
+        view.layer.shadowRadius = 15
+        view.layer.cornerRadius = 12
+        view.layer.maskedCorners = [.layerMinXMaxYCorner, .layerMaxXMaxYCorner]
+        return view
+    }()
+    
+    let doctorImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFill
+        imageView.clipsToBounds = true
+        imageView.layer.cornerRadius = 8
+        return imageView
+    }()
+    
+    let doctorInfo = DoctorInfoHorizontalView()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupUI()
+        setupConstrains()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setupUI() {
+        addSubview(doctorInfoView)
+        doctorInfoView.addSubview(doctorImageView)
+    }
+    
+    private func setupConstrains() {
+        [doctorInfoView, doctorImageView].forEach {
+            $0.translatesAutoresizingMaskIntoConstraints = false
+        }
+        
+        NSLayoutConstraint.activate([
+            doctorInfoView.topAnchor.constraint(equalTo: self.bottomAnchor),
+            doctorInfoView.leadingAnchor.constraint(equalTo: self.leadingAnchor),
+            doctorInfoView.trailingAnchor.constraint(equalTo: self.trailingAnchor),
+            doctorInfoView.heightAnchor.constraint(equalToConstant: 108),
+            
+            doctorImageView.centerYAnchor.constraint(equalTo: doctorInfoView.centerYAnchor),
+            doctorImageView.leadingAnchor.constraint(equalTo: doctorInfoView.leadingAnchor, constant: 8),
+            doctorImageView.heightAnchor.constraint(equalToConstant: 92),
+            doctorImageView.widthAnchor.constraint(equalToConstant: 92),
+            
+            doctorInfo.centerYAnchor.constraint(equalTo: doctorInfoView.centerYAnchor),
+            doctorInfo.trailingAnchor.constraint(equalTo: doctorInfoView.trailingAnchor, constant: -8),
+        ])
+    }
+
+}
+
